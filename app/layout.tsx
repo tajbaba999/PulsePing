ba4c5ee4b9ee11354971988f1a0e31dd0da50638
tajbaba999 <tajbaba153@gmail.com>
2025-12-15 18:45:39 +0530

    fix: fixed supressed warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
             {children}
